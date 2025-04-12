8e54c5feaa2deeb81eecc09cea9e8e6c5106dda9
Dnthe-05 <dongocthe2005cm@gmail.com>
2025-04-12 21:51:44 +0700

    Thêm hình ảnh

--- GUI_Website/DangNhap.cs
+++ GUI_Website/DangNhap.cs
@@ -21,7 +21,7 @@ namespace GUI_Website
             {
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new DangNhap()); 
+                Application.Run(new QuanLy_SanPham()); 
             }
         }
         public DangNhap()
